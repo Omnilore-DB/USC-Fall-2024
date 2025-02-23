37ccd3dccfd205bad49704ee37e5bed0573b2103
Brendon Zimmer <bzimmer@usc.edu>
2025-02-23 05:18:28 -0800

    remove test add ping

--- app/api/cron/test/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  // wait 20 secs then respond
-  console.log("Waiting for 20 seconds...");
-  await new Promise(resolve => setTimeout(resolve, 16000));
-  console.log("Done");
-  return Response.json({ message: "Hello, world!" });
-}
