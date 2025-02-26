d4628772a9e09b17105b010d52ca0a684012c4d6
Taisia <tmertz@usc.edu>
2025-02-26 13:53:06 -0800

    fixed row hover state

--- omnilore/components/ui/TableComponent.tsx
+++ omnilore/components/ui/TableComponent.tsx
@@ -22,8 +22,8 @@ const TableComponent = ({
 
     const handleRowClick = (pid: number) => {
         if (localSelectedRow !== pid) {
-            setLocalSelectedRow(pid); // Instantly change the row color
-            handleRowSelection(pid); // Notify parent component asynchronously
+            setLocalSelectedRow(pid);
+            handleRowSelection(pid);
         }
     };
 
@@ -54,57 +54,73 @@ const TableComponent = ({
                 <tbody>
                     {entries.map((item, index) => (
                         <tr
-                            key={index}
-                            className={`group cursor-pointer ${
-                                localSelectedRow === item[primaryKey] 
-                                    ? 'bg-gray-100' 
-                                    : 'hover:bg-gray-50'
-                            }`}
-                            onClick={() => handleRowClick(item[primaryKey])}
-                        >
-                            {roles.includes("admin") || roles.includes("treasurer") || roles.includes("registrar") ? (
-                                <>
-                                    <td className={`px-4 py-2 w-10 text-center sticky left-0 z-5 outline outline-1 outline-gray-200
-                                            ${localSelectedRow === item[primaryKey] 
-                                                ? 'bg-gray-100' 
-                                                : 'hover:bg-gray-50 bg-white'
-                                            }`}
-                                    >
-                                        <input
-                                            type="radio"
-                                            name="row-selection"
-                                            checked={localSelectedRow === item[primaryKey]}
-                                            onClick={(e) => e.stopPropagation()}
-                                            onChange={() => handleRowClick(item[primaryKey])}
-                                        />
-                                    </td>
-
-                                    <td 
-                                        className={`outline outline-gray-200 outline-1 px-4 py-2 sticky left-20 z-5
-                                            ${localSelectedRow === item[primaryKey] 
-                                                ? 'bg-gray-100' 
-                                                : 'hover:bg-gray-50 bg-white'
-                                            }`}
-                                    >
-                                        {item[primaryKey]}
-                                    </td>
-
-                                    {Object.keys(item)
-                                        .filter(name => name !== primaryKey)
-                                        .map(columnName => (
-                                            <td key={columnName} className="border border-gray-200 px-4 py-2">
-                                                {item[columnName]}
-                                            </td>
-                                        ))}
-                                </>
-                            ) : (
-                                Object.keys(item).map(columnName => (
-                                    <td key={columnName} className="border border-gray-200 px-4 py-2">
-                                        {item[columnName]}
-                                    </td>
-                                ))
-                            )}
-                        </tr>
+                        key={index}
+                        className={`group cursor-pointer ${
+                            localSelectedRow === item[primaryKey] 
+                                ? 'bg-gray-100' 
+                                : 'bg-white group-hover:bg-gray-50'
+                        }`}
+                        onClick={() => handleRowClick(item[primaryKey])}
+                    >
+                        {roles.includes("admin") || roles.includes("treasurer") || roles.includes("registrar") ? (
+                            <>
+                                <td 
+                                    className={`px-4 py-2 w-10 text-center sticky left-0 z-5 outline outline-1 outline-gray-200
+                                        ${localSelectedRow === item[primaryKey] 
+                                            ? 'bg-gray-100' 
+                                            : 'bg-white group-hover:bg-gray-50'
+                                        }`}
+                                >
+                                    <input
+                                        type="radio"
+                                        name="row-selection"
+                                        checked={localSelectedRow === item[primaryKey]}
+                                        onClick={(e) => e.stopPropagation()}
+                                        onChange={() => handleRowClick(item[primaryKey])}
+                                    />
+                                </td>
+                    
+                                <td 
+                                    className={`outline outline-gray-200 outline-1 px-4 py-2 sticky left-20 z-5
+                                        ${localSelectedRow === item[primaryKey] 
+                                            ? 'bg-gray-100' 
+                                            : 'bg-white group-hover:bg-gray-50'
+                                        }`}
+                                >
+                                    {item[primaryKey]}
+                                </td>
+                    
+                                {Object.keys(item)
+                                    .filter(name => name !== primaryKey)
+                                    .map(columnName => (
+                                        <td 
+                                            key={columnName} 
+                                            className={`border border-gray-200 px-4 py-2
+                                                ${localSelectedRow === item[primaryKey] 
+                                                    ? 'bg-gray-100' 
+                                                    : 'bg-white group-hover:bg-gray-50'
+                                                }`}
+                                        >
+                                            {item[columnName]}
+                                        </td>
+                                    ))}
+                            </>
+                        ) : (
+                            Object.keys(item).map(columnName => (
+                                <td 
+                                    key={columnName} 
+                                    className={`border border-gray-200 px-4 py-2
+                                        ${localSelectedRow === item[primaryKey] 
+                                            ? 'bg-gray-100' 
+                                            : 'bg-white group-hover:bg-gray-50'
+                                        }`}
+                                >
+                                    {item[columnName]}
+                                </td>
+                            ))
+                        )}
+                    </tr>
+                    
                     ))}
                     {entries.length === 0 && (
                         <tr>
