c1a8e0099f8b518972eef60d7ee1dc6ffe123ff8
Taisia <tmertz@usc.edu>
2025-03-28 11:16:51 -0700

    recovery

---
+++ app/admin/reports/page.tsx
@@ -0,0 +1,8 @@
+export default function ReportsPage() {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-2xl font-bold">Reports Page</h1>
+      </div>
+    );
+  }
+  
