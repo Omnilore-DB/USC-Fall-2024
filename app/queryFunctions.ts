7d5b11b6c7e07f513a5a9a82fe527abcea7325e7
Sanya Gupta <sanyagup@usc.edu>
2026-02-26 14:07:06 -0800

    bug fix: path error

--- app/queryFunctions.ts
+++ app/queryFunctions.ts
@@ -1,5 +1,5 @@
 import { supabase } from "@/app/supabase";
-import { Database } from "@/types/supabase";
+import { Database } from "./api/cron/src/supabase/types";
 
 export type TableName = keyof Database["public"]["Tables"] | "audit_logs";
 
